6c5bc7d5c0eaf5b6debb0e09643b0806caf057ec
Mohamed Ikhwan <1khwan.m0@gmail.com>
2019-01-18 23:43:21 +0530

    Create Born

--- my-portfolio/src/components/Born.js
+++ my-portfolio/src/components/Born.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import './Born.scss'
-
+import isles from '../resources/isles.png'
 class Born extends Component {
   render() {
-    return <div>Born</div>
+    return (
+      <div className="born-container">
+        <div className="img-container">
+          <img alt="isles" className="isles" src={isles} />
+        </div>
+        <div className="text-container">
+          <div className="h1">born</div>
+          <div className="h1">Maldivian</div>
+          <div className="h2">1994</div>
+        </div>
+      </div>
+    )
   }
 }
 
