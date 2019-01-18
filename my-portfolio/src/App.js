f56258bb71dd69351d4a1087ddeb1632c19485b5
Mohamed Ikhwan <1khwan.m0@gmail.com>
2019-01-18 21:25:20 +0530

    Clean up Transitions

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -22,7 +22,9 @@ class App extends Component {
     super(props)
     this.state = {
       currentPage: 0,
-      animating: false,
+      nextPage: 0,
+      scrollDirection: +1,
+      animating: true,
       componentState: [false, false, false, false, false, false]
     }
   }
@@ -44,21 +46,24 @@ class App extends Component {
   }
 
   toggleAnimating = () => {
-    this.setState((prev, props) => {
-      return {
-        animating: true
-      }
-    }, console.log('animation toggled to :', this.state.animating))
+    let { animating } = this.state
+    this.setState(
+      {
+        animating: !animating
+      },
+      console.log('animation toggled to :', this.state.animating)
+    )
   }
 
-  updateCurrentPage = scrollDirection => {
-    let { currentPage } = this.state
-    this.setState((prev, props) => {
-      return {
-        currentPage: currentPage + scrollDirection
-      }
-    })
-  }
+  // first render happens through on componentDidMount()
+  // on mouse scroll, check for which direction and whether we're above or below the page count ./
+  // update scrollDirection ./
+  // update nextPage ./
+
+  // trigger the currentPage fading out ./
+
+  // currentPage fading out completing triggers next Page fading in
+  // on nextPage entering, set nextPage page as currentPage
 
   handleOnScroll = e => {
     var { animating, currentPage } = this.state
@@ -66,46 +71,66 @@ class App extends Component {
       -1,
       Math.min(1, e.wheelscrollDirection || e.wheelDelta)
     )
-    // var scrollDirection = e
+    // inverse the direction because:
+    // I need it index to +1 on scroll down
+    scrollDirection *= -1
     console.log(scrollDirection, 'scrolldirection')
     if (!animating && currentPage < 5 && scrollDirection > 0) {
-      console.log('firinghandleonscroll')
-      // if scrolling down
-      // toggle Animating to true.
-      this.triggerTransition(currentPage, currentPage + scrollDirection)
-      // fade out current page
-      // fade in next page
-      // toggle animating to false.
-      this.updateCurrentPage(scrollDirection)
+      console.log('scrolled Down')
+      this.toggleAnimating()
+      this.updateScrollDirection(scrollDirection)
+      this.updateNextPage(scrollDirection)
+      this.triggerTransition(currentPage)
     } else if (!animating && currentPage >= 1 && scrollDirection < 0) {
-      // if scrolling up
-      this.triggerTransition(currentPage, currentPage + scrollDirection)
-      this.updateCurrentPage(scrollDirection)
+      console.log('scrolled Up')
+      this.toggleAnimating()
+      this.updateScrollDirection(scrollDirection)
+      this.updateNextPage(scrollDirection)
+      this.triggerTransition(currentPage)
     }
   }
 
-  triggerTransition = (current, next) => {
-    // get components array
-    console.log(current, next)
+  updateScrollDirection = direction => {
+    this.setState(
+      {
+        scrollDirection: direction
+      },
+      console.log('direction state:', direction)
+    )
+  }
+  updateNextPage = scrollDirection => {
+    let { nextPage } = this.state
+    this.setState(
+      {
+        nextPage: nextPage + scrollDirection
+      },
+      console.log('nextPage state:', nextPage)
+    )
+  }
+
+  triggerTransition = index => {
     let { componentState } = this.state
-    console.log(this.state.componentState)
-    // transition out
-    componentState[current] = false
-    // transition in
-    componentState[next] = true
-    // update state
+    componentState[index] = !componentState[index]
     this.setState(
       {
         componentState: componentState
       },
-      console.log(this.state.componentState)
+      console.log('componentState state:', componentState)
     )
   }
 
-  buttonPush = () => {
-    this.setState({
-      componentState: [false, true]
-    })
+  triggerNextTransition = () => {
+    let { nextPage, componentState } = this.state
+    componentState[nextPage] = !componentState[nextPage]
+    this.setState(
+      {
+        componentState: componentState,
+        // i feel uncomfortable that its doing two things but,
+        // CURRENT PAGE is updated here.
+        currentPage: nextPage
+      },
+      console.log('componentState state:', this.state)
+    )
   }
 
   render() {
@@ -120,6 +145,8 @@ class App extends Component {
           timeout={300}
           classNames="intro"
           unmountOnExit
+          onEntered={this.toggleAnimating}
+          onExited={this.triggerNextTransition}
         >
           <Intro />
         </CSSTransition>
@@ -130,6 +157,8 @@ class App extends Component {
           timeout={300}
           classNames="intro"
           unmountOnExit
+          onEntered={this.toggleAnimating}
+          onExited={this.triggerNextTransition}
         >
           <Born />
         </CSSTransition>
