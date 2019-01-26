d924bfd4ec374684eec8b9137d2dcec5ca5538ac
Mohamed Ikhwan <1khwan.m0@gmail.com>
2019-01-26 12:19:29 +0530

    Finished Gaming

--- my-portfolio/src/components/Gaming.js
+++ my-portfolio/src/components/Gaming.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react'
 import './Gaming.scss'
+import { TimelineLite, TweenLite } from 'gsap/all'
 import controller from '../resources/controller.png'
+
 class Gaming extends Component {
+  constructor(props) {
+    super(props)
+
+    this.timeline = new TimelineLite({ paused: true })
+    this.myElements = []
+  }
+  componentDidMount() {
+    this.timeline
+      .from(this.myElements[0], 0.6, { y: -10, opacity: 0 })
+      .add([
+        TweenLite.to(this.myElements[1], 0.4, { y: -10, opacity: 1 }),
+        TweenLite.to(this.myElements[2], 0.4, {
+          y: -10,
+          opacity: 1,
+          delay: 0.1
+        })
+      ])
+      .to(this.myElements[3], 0.5, { y: -10, opacity: 1 })
+
+      // .seek(5)
+      .play()
+  }
   render() {
     return (
       <div className="page gaming-container">
         <div>
-          <img alt="controller" className="controller" src={controller} />
+          <img
+            ref={div => (this.myElements[0] = div)}
+            alt="controller"
+            className="controller"
+            src={controller}
+          />
         </div>
         <div className="text-container">
-          <div className="h1">grew up</div>
-          <div className="h2">Gaming</div>
+          <div className="text">
+            <div ref={div => (this.myElements[1] = div)} className="h1">
+              grew
+            </div>
+
+            <div ref={div => (this.myElements[2] = div)} className="h1">
+              up
+            </div>
+          </div>
+          <div ref={div => (this.myElements[3] = div)} className="h2">
+            Gaming
+          </div>
         </div>
       </div>
     )
